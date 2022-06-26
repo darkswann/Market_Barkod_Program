901a6205330b09c8cf3fe0eb9402b8be8848c3a3
ertugrull.gns <74158698+ertugrul37@users.noreply.github.com>
2022-06-27 00:56:21 +0300

    ekleme

---
+++ Market_Barkod_Programı/pages/Aylik_Satis_Raporu.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Market_Barkod_Programı.pages
+{
+    public partial class Aylik_Satis_Raporu : DevExpress.XtraEditors.XtraForm
+    {
+        public Aylik_Satis_Raporu()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Market_Barkod_Programı/pages/Kasa_Listesi.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Market_Barkod_Programı.pages
+{
+    public partial class Kasa_Listesi : DevExpress.XtraEditors.XtraForm
+    {
+        public Kasa_Listesi()
+        {
+            InitializeComponent();
+        }
+    }
+}
